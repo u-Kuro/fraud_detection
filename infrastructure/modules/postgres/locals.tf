2c055d3fe37e3a93a984b9a514cd45c32553ed73
Reaven Dupitas <dupitas.reaven@gmail.com>
2026-08-27 14:35:56 +0800

    ongoing terraform changes...

--- infrastructure/modules/postgres/locals.tf
+++ infrastructure/modules/postgres/locals.tf
@@ -2,8 +2,8 @@ locals {
   # RDS
   # /teams
   rds_postgres_teams_schemas             = { for v in var.rds_postgres_teams : v => v }
-  rds_postgres_teams_usernames           = { for v in var.rds_postgres_teams : v => v }
-  rds_postgres_teams_passwords           = { for v in var.rds_postgres_teams : v => v }
-  rds_postgres_teams_migration_usernames = { for v in var.rds_postgres_teams : v => "${v}-migration" }
-  rds_postgres_teams_migration_passwords = { for v in var.rds_postgres_teams : v => "${v}-migration" }
+  rds_postgres_teams_usernames           = { for v in var.rds_postgres_teams : v => "${v}-0123456789" }
+  rds_postgres_teams_passwords           = { for v in var.rds_postgres_teams : v => "${v}-0123456789" }
+  rds_postgres_teams_migration_usernames = { for v in var.rds_postgres_teams : v => "${v}-migration-0123456789" }
+  rds_postgres_teams_migration_passwords = { for v in var.rds_postgres_teams : v => "${v}-migration-0123456789" }
 }
